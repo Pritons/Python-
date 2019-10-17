8fe47b17dc59105c21e5e90073b8cc5f09acad7a
Pritons <55536024+Pritons@users.noreply.github.com>
2019-10-17 11:35:38 +0200

    Again again

--- Firststeps/again.py
+++ Firststeps/again.py
@@ -1,8 +1,13 @@
-def repeat(message):
+def repeat():
 
     finish = False
     while finish == False:
         m=input("Do you want to continue (yes/no) ")
-        if (m !="yes"):
-            print("I don't understand you, sorry!")
+        if (m =="yes"):
+            print("Try agin.")
+
+        elif (m =="no"):
             finish = True
+        else:
+            print("I don't understand you, sorry!")
+repeat()
