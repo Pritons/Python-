5373140457f54a76ca0dd8bc9a5e6d98c03948ba
Pritons <55536024+Pritons@users.noreply.github.com>
2019-10-24 11:37:34 +0200

    list

--- Firststeps/list.py
+++ Firststeps/list.py
@@ -1,9 +1,10 @@
 import random
 def randintlist(len, ceiling):
-    differentlist = list()
+    list1 = list()
 
     for x in range (len):
         number = random.randint(1, ceiling)
         list1.append(number)
+    return list1
 
 print(randintlist(5, 10))
