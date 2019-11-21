47a48e7952a3efce210af87ecd47bdd324cbd890
Pritons <55536024+Pritons@users.noreply.github.com>
2019-11-21 11:53:22 +0100

    times1

--- Firststeps/times1.py
+++ Firststeps/times1.py
@@ -5,3 +5,8 @@ ceiling = int(input())
 number1 = random.randint(1, ceiling)
 number2 = random.randint(1, ceiling)
 print("how much is ",number1,"*",number2,"?")
+answer = int(input())
+if(answer == number1*number2):
+    print("The answer is correct!")
+else:
+    print("The answer is not correct. The correct answer is",answer)
