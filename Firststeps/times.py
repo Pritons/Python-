a28e7ba598eea63f71aa06cdb9c6b812829dfb52
Pritons <55536024+Pritons@users.noreply.github.com>
2019-11-28 11:58:16 +0100

    list

--- Firststeps/times.py
+++ Firststeps/times.py
@@ -6,9 +6,21 @@ many = int(input())
 print("ceiling?")
 ceiling = int(input())
 
-
+listnumber1=list()
+listnumber2=list()
+listanswer=list()
 for x in range (many):
     number1 = random.randint(1, ceiling)
     number2 = random.randint(1, ceiling)
+    listnumber1.append(number1)
+    listnumber2.append(number2)
     print("how much is ",number1,"*",number2,"?")
     answer = int(input())
+    listanswer.append(answer)
+for i in range (many):
+    q=str(listnumber1[i]),"*",str(listnumber2[i])
+    print("Q"+str(i+1)+q)
+
+m = input("Want to try again (yes/no)? ")
+if (m != "yes"):
+    finish = True
